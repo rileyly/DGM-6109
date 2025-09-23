1c91bec2160b7dcd554375b2c1e8a6b8c8d9b02c
Riley Lyles <lyles.ko@northeastern.edu>
2025-09-22 17:53:10 -0700

    Lab 1 - F to C Temperature converter

---
+++ lab1/temp-conversion.js
@@ -0,0 +1,15 @@
+let fahrenheit = 79 // 0C, 273.15K
+// July in Washington, D.C. 
+console.log("Temperature (Fahrenheit): " + fahrenheit + " degrees") 
+
+// calculate celsius and store in celsius variable
+// print that value
+
+let celsius = (fahrenheit - 32) / 1.8
+console.log("Temperature (Celsius): " + celsius + " degrees")
+
+// calculate kelvin and store in variable
+// print that value
+
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+console.log("Temperature (Kelvin): " + kelvin + " degrees")
